53b6f60954e90ed1ed8fcbb327c4ac955d482ef4
azurata <git@azurata.me>
2022-12-18 02:39:27 +0900

    fix: calc

--- backend/position.js
+++ backend/position.js
@@ -1,10 +1,10 @@
 const r = 6378.137;//赤道半径
 
-module.exports = (x1, y1, x2, y2) => {
+module.exports = (lng1, lat1, lng2, lat2) => {
   let d, phai; //dが距離, phaiが方位角
 
-  d = r * Math.acos(Math.sin(y1) * Math.sin(y2) + Math.cos(y1) * Math.cos(y2) * Math.cos(x2 - x1));
-  phai = 90 - Math.atan(Math.sin(x1 - x2) / (Math.cos(y1) * Math.tan(y2) - Math.sin(y1) * Math.cos(x2 - x1)));
+  d = r * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1));
+  phai = 90 - Math.atan(Math.sin(lng2 - lng1) / (Math.cos(lat1) * Math.tan(lat2) - Math.sin(lat1) * Math.cos(lng2 - lng1)));
   phai = isNaN(phai) ? 0 : phai
 
   return [d, phai]
